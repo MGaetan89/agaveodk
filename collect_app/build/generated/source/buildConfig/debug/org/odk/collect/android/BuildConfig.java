14a3a5735c116848657d07fc3f68c2377cfad74b
Aurora Sánchez <aurora.sanchez@live.com.mx>
2023-04-10 11:59:56 -0600

    Config.

--- collect_app/build/generated/source/buildConfig/debug/org/odk/collect/android/BuildConfig.java
+++ collect_app/build/generated/source/buildConfig/debug/org/odk/collect/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "org.odk.collect.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1069;
-  public static final String VERSION_NAME = "4ec93dd-dirty";
+  public static final int VERSION_CODE = 1070;
+  public static final String VERSION_NAME = "75285e9-dirty";
 }
